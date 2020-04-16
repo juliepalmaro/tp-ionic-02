fcaabe0bff6bd509fa3a17a1374694e42b7eb878
C:\xampp\htdocs <julie.palmaro@gmail.com>
2020-04-16 10:33:51 +0200

    add geolocation to tests

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -10,7 +11,8 @@ describe('HomePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [Geolocation]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
