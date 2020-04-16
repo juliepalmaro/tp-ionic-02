3383895b32bdaf94a686fe2cd9fbff8894910839
C:\xampp\htdocs <julie.palmaro@gmail.com>
2020-04-16 12:19:13 +0200

    page camera + link

---
+++ src/app/camera/camera.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+
+import { CameraPage } from './camera.page';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: CameraPage
+      }
+    ])
+  ],
+  declarations: [CameraPage],
+})
+export class CameraPageModule {}
